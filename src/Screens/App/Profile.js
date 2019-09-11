0cb154fd979898c4067652640e43d89507520f2c
Fuad Mustamirrul Ishlah 83 <amir24405@gmail.com>
2019-09-11 13:24:39 +0700

    adding user profile

--- src/Screens/App/Profile.js
+++ src/Screens/App/Profile.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Text } from 'react-native';
+import { Text, StyleSheet, View, Image } from 'react-native';
 import firebase from 'firebase';
 import firebaseConfig from '../../Config/firebase';
-import { Button } from 'native-base';
+import { Button, Icon, Input } from 'native-base';
+import StackedLabelTextbox from '../../Components/StackedLabelTextbox';
 
 export default class ChatRoom extends React.Component{
   
+  constructor(){
+    super()
+    this.state = {
+      user : firebase.auth().currentUser
+    }
+  }
+
   handleLogout = () => {
     firebase.auth().signOut()
       .then(()=>{
@@ -15,8 +23,50 @@ export default class ChatRoom extends React.Component{
         console.error(err)
       })
   }
-  
+
   render(){
-    return <Button onPress={this.handleLogout}><Text>Sign Out</Text></Button>
+    return (
+      <View style={{height:'100%'}}>
+        <View style={styles.profileHeader}/>
+        <View style={{marginTop:-75, marginBottom:50}}>
+          <Image source={{uri:this.state.user.photoURL}} style={styles.profileImage} />
+          <Text style={styles.profileTitle}>Username</Text>
+          <View style={styles.profileValue}>
+            <Text style={styles.profileValueText}>{this.state.user.displayName}</Text><Icon onPress={()=>{this.setState({editingUsername:true})}} type="MaterialIcons" name="edit" />
+          </View>
+          <Text style={styles.profileTitle}>Email</Text>
+          <View style={styles.profileValue}>
+            <Text style={styles.profileValueText}>{this.state.user.email}</Text>
+          </View>
+        </View>
+        <Button block onPress={this.handleLogout}><Text>Logout</Text></Button>
+      </View>
+    )
   }
 }
+const styles = StyleSheet.create({
+  profileHeader:{
+    height:150, 
+    backgroundColor:'#2196f3'
+  },
+  profileImage:{
+    width:150, 
+    height:150, 
+    resizeMode:'contain', 
+    alignSelf:'center',
+  },
+  profileTitle:{
+    fontSize:25, 
+    marginLeft:20
+  },
+  profileValue:{
+    paddingHorizontal:40,
+    width:'100%',
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  profileValueText:{
+    fontSize:20, 
+
+  }
+})
